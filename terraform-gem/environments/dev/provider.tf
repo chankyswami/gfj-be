f8b41628ff1fcc0022772a56405825ae72371002
chankyswami <c.innovator@gmail.com>
2025-08-24 16:22:36 +0530

    added terraform

---
+++ terraform-gem/environments/dev/provider.tf
@@ -0,0 +1,5 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "ap-south-1"
+  profile = "default"
+}
---
+++ terraform-gem/environments/dev/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id_1 = "ami-02bf868c59ec426d4"
+ami_id_2 = "ami-0d8e035f70bf3d8b5"
